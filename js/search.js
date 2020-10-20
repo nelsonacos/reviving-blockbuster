2b6ae0f7ca8051f7712465d030210287d78f613a
Nelson Acosta <nelson.acosta.web@gmail.com>
2020-10-20 01:27:28 -0300

    feat(js):Añadido buscador.

---
+++ js/search.js
@@ -0,0 +1,37 @@
+import movies from './movies.js'
+import render from './render.js'
+
+const search = window['search-form']
+
+search.addEventListener('submit', function (event) {
+    event.preventDefault()
+    const formData = new FormData(this)
+    const query = formData.get('title')
+    const movies = searchMovie(query)
+    console.log(movies)
+    if (movies) {
+        return render(movies)
+    }
+    return alert('No enconramos tu película')
+})
+
+function filterByTitle(title) {
+    return movies.filter((movie) => {
+        return movie.title.toLowerCase().includes(title.toLowerCase())
+    })
+}
+
+function findById(id) {
+    // return movies.filter((movie) => {
+    return movies.find((movie) => {
+        return movie.id === parseInt(id, 10)
+    })
+}
+
+function searchMovie(query) {
+    if (isNaN(query)) {
+        return filterByTitle(query)
+    }
+    return [findById(query)]
+    // return findById(query)
+}
